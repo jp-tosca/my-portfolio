c80413a52601dc2e9a29fee9fe6719eb1bcec382
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-07-16 00:39:04 -0400

    feat: update project descriptions for Dataverse Hub and Dataverse Marketplace

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -103,22 +103,17 @@ export const projects = [
     title: "Dataverse Hub",
     github: "https://github.com/IQSS/dataverse-hub",
     description: [
-      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
-      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
-      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
-      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
-      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+      "Centralized platform for metrics and analytics of Dataverse repositories.",
+      "Track of statistics about 120+ repositories, 1.5M+ datasets, and 2.5M+ files.",
+      "Built with Spring Boot, PostgreSQL, and OpenAPI.",      
     ],
   },
   {
     title: "Dataverse Marketplace",
     github: "https://github.com/IQSS/dataverse-marketplace",
     description: [
-      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
-      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
-      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
-      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
-      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+      "A platform to distribute and install applications and external tools on Dataverse installations.",
+      "Built with React, Spring Boot, React, PostgreSQL, and OpenAPI.",
     ],
   },
 ];
